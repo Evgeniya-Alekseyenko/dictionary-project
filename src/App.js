3548505d7af989ed1352d1d948fd5943060a1512
Evgeniya-Alekseyenko <79755777+Evgeniya-Alekseyenko@users.noreply.github.com>
2022-10-01 10:59:22 +0300

    Created the Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,15 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 function App() {
     return (
         <div className='App'>
             <header className='App-header'>
-                <div className='container shadow min-vh-100 py-4'>
-                    <h1 className='text-center m-4'> DICTIONARY</h1>
-                    <div className='row'>
-                        <div className='col-md-5 mx-auto'>
-                            <div class='small fw-light'>
-                                What word do you want to look up?
-                            </div>
-                            <div className='input-group'>
-                                <input
-                                    className='form-control border-end-0 border rounded-pill'
-                                    type='search'
-                                    value='search'
-                                    id='example-search-input'
-                                />
-                                <span className='input-group-append'>
-                                    <button
-                                        className='btn btn-outline-secondary bg-white border-bottom-0 border rounded-pill ms-n5'
-                                        type='button'
-                                    >
-                                        <i className='fa fa-search'></i>
-                                    </button>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <Dictionary />
             </header>
+            <footer className='text-center'>
+                <small>Coded by Yevgeniya Alekseyenko</small>
+            </footer>
         </div>
     );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Dictionary.css';
+
+export default function Dictionary() {
+    return (
+        <div className='dictionary'>
+            <div className='container shadow min-vh-100 py-4'>
+                <h1 className='text-center m-4'> DICTIONARY</h1>
+                <div className='row'>
+                    <div className='col-md-5 mx-auto'>
+                        <div className='small fw-light'>
+                            What word do you want to look up?
+                        </div>
+                        <div className='input-group'>
+                            <input
+                                className='form-control border-end-0 border rounded-pill'
+                                type='search'
+                                // value='search'
+                                // id='example-search-input'
+                            />
+                            <span className='input-group-append'>
+                                <button
+                                    className='btn btn-outline-secondary bg-white border-bottom-0 border rounded-pill ms-n5'
+                                    type='button'
+                                >
+                                    <i className='fa fa-search'></i>
+                                </button>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
